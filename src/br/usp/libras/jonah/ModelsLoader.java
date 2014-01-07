59044f03117f44aa96e22115118fc90a56305bd4
Leonardo Leite <leonardofl87@gmail.com>
2014-01-06 23:20:37 -0200

    novas configurações de mão feitas pela Lucy

--- src/br/usp/libras/jonah/ModelsLoader.java
+++ src/br/usp/libras/jonah/ModelsLoader.java
@@ -20,27 +20,26 @@ public class ModelsLoader {
 
     public static final String MODELS_PATH = "resources/models/";
 
-    private static HandShape[] HAND_MODELS_TO_LOAD = new HandShape[] {HandShape.ANGULO_ABERTO_AFASTADO, 
-    	HandShape.ANGULO_ABERTO, HandShape.ANGULO_COM_POLEGAR, HandShape.ANGULO_FECHADO, HandShape.CURVADOS, 
-    	HandShape.CURVADO_X, HandShape.FLEXIONADO1, HandShape.FLEXIONADO2, HandShape.INDICADOR, HandShape.MAO_2, 
-    	HandShape.MAO_3, HandShape.MAO_4, HandShape.MAO_5_COM_POLEGAR_PARA_FRENTE, HandShape.MAO_5, HandShape.MAO_A, 
-    	HandShape.MAO_BANHEIRO, HandShape.MAO_B, HandShape.MAO_C_COM_POLEGAR_PARA_O_LADO, HandShape.MAO_CERTO_ABERTA, 
-    	HandShape.MAO_CERTO, HandShape.MAO_C, HandShape.MAO_D, HandShape.MAO_DOENTE, 
-    	HandShape.MAO_ESTENDIDA_COM_POLEGAR, HandShape.MAO_ESTICADA, HandShape.MAO_F, HandShape.MAO_JESUS, 
-    	HandShape.MAO_K, HandShape.MAO_L_COM_C, HandShape.MAO_L, HandShape.MAO_ONZE, 
-    	HandShape.MAO_O, HandShape.MAO_PASSARINHO_ABERTA, HandShape.MAO_PASSARINHO_FECHADA, HandShape.MAO_R, 
-    	HandShape.MAO_S, HandShape.MAO_SO, HandShape.MAO_U, HandShape.MAO_Y, HandShape.O_CURVADO, 
-    	HandShape.U_COM_POLEGAR, HandShape.ANGULO_ABERTO_AFASTADO, HandShape.ANGULO_ABERTO, 
-    	HandShape.ANGULO_COM_POLEGAR, HandShape.ANGULO_FECHADO, HandShape.CURVADOS, HandShape.CURVADO_X, 
-    	HandShape.FLEXIONADO1, HandShape.FLEXIONADO2, HandShape.INDICADOR, HandShape.MAO_2, HandShape.MAO_3, 
-    	HandShape.MAO_4, HandShape.MAO_5_COM_POLEGAR_PARA_FRENTE, HandShape.MAO_5, HandShape.MAO_A, 
-    	HandShape.MAO_BANHEIRO, HandShape.MAO_B, HandShape.MAO_C_COM_POLEGAR_PARA_O_LADO, HandShape.MAO_CERTO_ABERTA, 
-    	HandShape.MAO_CERTO, HandShape.MAO_C, HandShape.MAO_D, HandShape.MAO_DOENTE, 
-    	HandShape.MAO_ESTENDIDA_COM_POLEGAR, HandShape.MAO_ESTICADA, HandShape.MAO_F, HandShape.MAO_JESUS, 
-    	HandShape.MAO_K, HandShape.MAO_L_COM_C, HandShape.MAO_L, HandShape.MAO_ONZE, 
-    	HandShape.MAO_O, HandShape.MAO_PASSARINHO_ABERTA, HandShape.MAO_PASSARINHO_FECHADA, HandShape.MAO_R, 
-    	HandShape.MAO_S, HandShape.MAO_SO, HandShape.MAO_U, HandShape.MAO_Y, HandShape.O_CURVADO, 
-    	HandShape.U_COM_POLEGAR} ;
+    private static HandShape[] HAND_MODELS_TO_LOAD = new HandShape[] {HandShape._3DEDOS_O, 
+    	HandShape.ANGULO_ABERTO_AFASTADO, HandShape.ANGULO_ABERTO, HandShape.ANGULO_COM_POLEGAR, 
+    	HandShape.ANGULO_FECHADO, HandShape.ANGULO_SEM_POLEGAR, HandShape.C_AFASTADO, 
+    	HandShape.CURVADOS_COM_POLEGAR, HandShape.CURVADOS, HandShape.CURVADO_U, 
+    	HandShape.CURVADO_X, HandShape.FLEXIONADO1, HandShape.FLEXIONADO2, HandShape.FLEXIONADO3, 
+    	HandShape.INDICADOR, HandShape.MAO_2, HandShape.MAO_3_6, HandShape.MAO_3, 
+    	HandShape.MAO_3_PARA_FRENTE, HandShape.MAO_4, HandShape.MAO_5_COM_POLEGAR_PARA_FRENTE, 
+    	HandShape.MAO_5, HandShape.MAO_A, HandShape.MAO_BANHEIRO, HandShape.MAO_B, 
+    	HandShape.MAO_C_COM_POLEGAR_PARA_O_LADO, HandShape.MAO_CERTO_ABERTA, 
+    	HandShape.MAO_CERTO, HandShape.MAO_CERTO_RETA, HandShape.MAO_C, HandShape.MAO_D, 
+    	HandShape.MAO_DOENTE, HandShape.MAO_DROGA, HandShape.MAO_E, 
+    	HandShape.MAO_ESTENDIDA_COM_POLEGAR, HandShape.MAO_ESTICADA, HandShape.MAO_FIGA, 
+    	HandShape.MAO_F, HandShape.MAO_G, HandShape.MAO_I_LOVE_YOU, HandShape.MAO_I, 
+    	HandShape.MAO_JESUS, HandShape.MAO_K, HandShape.MAO_L_COM_C, HandShape.MAO_L_COM_O, 
+    	HandShape.MAO_L, HandShape.MAO_N, HandShape.MAO_ONZE_APERTA, HandShape.MAO_ONZE, 
+    	HandShape.MAO_O, HandShape.MAO_PASSARINHO_ABERTA, HandShape.MAO_PASSARINHO_FECHADA, 
+    	HandShape.MAO_PATO_ABERTA, HandShape.MAO_PATO_FECHADA, HandShape.MAO_PROFISSAO, 
+    	HandShape.MAO_REVOLVER, HandShape.MAO_R, HandShape.MAO_SETE, HandShape.MAO_S, 
+    	HandShape.MAO_SO, HandShape.MAO_TERCA, HandShape.MAO_T, HandShape.MAO_U, HandShape.MAO_W, 
+    	HandShape.MAO_Y, HandShape.O_CARACOL, HandShape.O_CURVADO, HandShape.U_COM_POLEGAR} ;
     
     private static Map<HandModel, AnimObj> map = new HashMap<HandModel, AnimObj>();
 
